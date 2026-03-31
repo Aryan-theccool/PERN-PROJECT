f31ad21c7899138f2327e140c7a621e50c1c0ae9
Aryan singh bhadoria <aryanbhadoria100@gmail.com>
2026-03-31 23:49:39 +0530

    last of day

--- quiet-clubs-wonder/src/pages/subject/list.tsx
+++ quiet-clubs-wonder/src/pages/subject/list.tsx
@@ -1,10 +1,13 @@
+import { Breadcrumb } from '@/components/refine-ui/layout/breadcrumb'
+import { ListView } from '@/components/refine-ui/views/list-view'
 import React from 'react'
 
 const list = () => {
   return (
-    <div>
-      
-    </div>
+    <ListView>
+      <Breadcrumb />
+      <h1 className="page_title">Subjects</h1>
+    </ListView>
   )
 }
 
